085af203e1ca2473d29b50012e6baf92b7e1f8cc
-v <ojeda.gian21@gmail.com>
2023-08-04 04:46:20 -0300

    utilizamos multer

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
     res.json('list');
 })
 app.use('/products', products);
-app.use('/api/users', users);
+app.use('/users', users);
+
+app.use(function (req, res, next) {
+    return res.status(404).json({
+        status: 404,
+        error: 'Resource not found',
+        message: 'Error en el recurso solicitado'
+    })
+});
 
 app.listen(4000, () => console.log('server corriendo en el puerto http://localhost:4000/'));
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,22 @@
+const path = require('path');
+const fs = require('fs');
 const express = require ('express');
+const multer = require ('multer');
 const router = express.Router();
 const controller = require ('../controllers/productsController');
 
-router.post('/crear', controller.crear);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, '../../public/image'))
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'image-${Date.now()}${path.etxname(file.originalname)}');
+    } 
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/crear', upload.single('image'), controller.crear);
 router.get('/listar', controller.list);
 router.get('/detalle/:id', controller.detalle);
 
